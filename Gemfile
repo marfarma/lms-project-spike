9ba471c1060d77a87ebc39e0e8672205aeed2c7d
pprice <pprice@sindexsystems.com>
2010-12-25 12:47:42 -0500

    User auth, profile

--- Gemfile
+++ Gemfile
@@ -1,11 +1,9 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.3'
-
-# Bundle edge Rails instead:
-# gem 'rails', :git => 'git://github.com/rails/rails.git'
-
 gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
+gem 'gravatar_image_tag', '1.0.0.pre2'
+
 
 group :development do
   gem 'rspec-rails', '2.3.1'
@@ -16,6 +14,7 @@ group :test do
   gem 'rspec', '2.3.0'
   gem 'webrat', '0.7.2'
   gem 'spork', '0.8.4'
+  gem 'factory_girl_rails', '1.0'
 end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101225063055) do
+ActiveRecord::Schema.define(:version => 20101225093700) do
 
   create_table "lessons", :force => true do |t|
     t.string   "title"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(:version => 20101225063055) do
     t.boolean  "mailable"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
 end
