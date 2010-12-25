0ee89af9057cc57eb61c5cf6308c223cec57d373
pprice <pprice@sindexsystems.com>
2010-12-24 23:53:12 -0500

    layout and routes done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  
+  def logo
+    logo = image_tag("logo.jpg", :alt => "Sample App", :class => "round")
+    link_to logo, root_path
+  end
+  
   # Return a title on a per-page basis.
   def title
     base_title = "Teaching Ministry Manager - Proposed OneBody Enhancement"
